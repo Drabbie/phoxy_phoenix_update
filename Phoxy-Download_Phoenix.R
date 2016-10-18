e433e6b6d9fd582e728623753de31f4ff494df92
Drabbie <alisha.kim@utdallas.edu>
2016-10-18 00:59:06 -0500

    Add files via upload
    Fix phoenix download script

---
+++ Phoxy-Download_Phoenix.R
@@ -0,0 +1,53 @@
+# get all the URLs on a page
+get_links <- function (phoenix_version = 'current') {
+  phoenix_version <- gsub('.', '', phoenix_version, fixed = T) # remove dots
+  
+  # check version user input, either 'current' or up to 3 digits
+  # with optional 'v' in the beginning
+  if (!grepl('(current|v?\\d{,3})', phoenix_version)) stop('Incorrect version name.')
+  
+  if (!grepl('^(v|current)', phoenix_version)) # if the user submitted a version without 'v'
+    phoenix_version <- paste0('v', phoenix_version)
+  
+  # Access the Phoenix API. 
+  url <- "http://phoenixdata.org/data"
+  page <- xml2::read_html(url)
+  table_links <- page %>%
+    rvest::html_nodes("a") %>%  #find all links
+    rvest::html_attr("href") %>%  #get the url
+    stringr::str_subset("\\.zip") %>%  #find those that end in zip
+    xml2::url_absolute(url)
+  
+  return(table_links)
+}
+
+
+# given a link, download the file and write it to the specified directory
+dw_file <- function(link, destpath) {
+  # extract filename from link
+  m <- regexpr('[^/]*(?=\\.zip$)', link, perl = T)
+  filename <- regmatches(link, m)
+  
+  # remove trailing filepath separator to destpath if it's there
+  destpath <- gsub(paste0(.Platform$file.sep, '$'), '', destpath)
+  
+  # download method
+  if (.Platform$OS.type == 'windows')
+    download_method <- 'auto'
+  else
+    download_method <- 'curl'
+  
+  # download and unzip to destpath
+  temp <- tempfile()
+  download.file(link, temp, method = download_method, quiet = T)
+  unzip(temp, exdir = destpath)
+  unlink(temp)
+}
+
+#' @export
+#' @importFrom plyr l_ply progress_text
+download_phoenix <- function(destpath, phoenix_version = 'current'){
+  links <- get_links(phoenix_version = phoenix_version)
+  message("Downloading and unzipping files.")
+  plyr::l_ply(links, dw_file, destpath = destpath, .progress = plyr::progress_text(char = '='))
+}
